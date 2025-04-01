ead4f569a0ca0e1056adb3db76ca86a124d70cd8
christiantomato <christianntamayo@gmail.com>
2025-04-01 12:06:15 -0400

    fix enum scope

--- src/include/token.h
+++ src/include/token.h
@@ -5,29 +5,38 @@
 - strings
 - operators 
 - punctuation
+etc.
 */
 
 #ifndef TOKEN_H
 #define TOKEN_H
 
+//enum for the types of tokens we will be classifying
+typedef enum token_type {
+    //enums start at index 0.
+    TOKEN_ID,
+    TOKEN_STRING,
+    TOKEN_NUM,
+    TOKEN_EQUALS,
+    TOKEN_SEMI,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN, 
+    TOKEN_ADD,
+    TOKEN_HYPHEN,
+    TOKEN_COLON
+} type;
+
 //tokens will be of the form (type, value)
 typedef struct token {
-    //enum for the types of tokens we will be classifying
-    enum token_type {
-        //enums start at index 0.
-        TOKEN_EQUALS,
-        TOKEN_SEMI,
-        TOKEN_LPAREN,
-        TOKEN_RPAREN,
-        TOKEN_STRING,
-        TOKEN_ID,
-        TOKEN_NUM
-    } type;
+    //token type
+    int type;
     //value token holds
     char* value;
 } token; 
 
 //declare the initializer for the token
 token* init_token(int type, char* value);
+//method to get the actual enum name which is helpful
+char* token_type_str(token* token);
 
 #endif
