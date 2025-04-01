2aec3dc6bd8f25aecb30c151da145a67aaf19d3b
christiantomato <christianntamayo@gmail.com>
2025-04-01 10:29:47 -0400

    include new methods

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -14,7 +14,7 @@ typedef struct lexer {
     char* contents;
 } lexer;
 
-//functions
+//methods
 
 //lexer initializor
 lexer* init_lexer(char* contents);
@@ -24,8 +24,12 @@ void advance(lexer* lexer);
 void skip_whitespace(lexer* lexer);
 //to get the next token
 token* tokenize_next(lexer* lexer);
-//function for how we will tokenize strings
+//method for how we will tokenize strings
 token* tokenize_string(lexer* lexer); 
+//method to tokenize words, like variables and keywords (but not strings!)
+token* tokenize_ID(lexer* lexer);
+//method to tokenize numbers
+token* tokenize_number(lexer* lexer);
 //continuing on a token (just advancing the lexer and returning our token to prepare for next tokenization)
 token* continue_with_token(lexer* lexer, token* token);
 //function to get the current lexer char as a string
