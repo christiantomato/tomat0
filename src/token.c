12660ef064ea7cf59cdb5000b0698ddae6e32611
christiantomato <christianntamayo@gmail.com>
2025-04-01 12:06:27 -0400

    implement tokentypestr()

--- src/token.c
+++ src/token.c
@@ -21,3 +21,21 @@ token* init_token(int type, char* value) {
         return NULL;
     }
 }
+
+//get the token type as a string
+char* token_type_str(token* token) {
+    //switch on the token type
+    switch(token->type) {
+        case TOKEN_ID: return "TOKEN_ID"; break;
+        case TOKEN_STRING: return "TOKEN_STRING"; break;
+        case TOKEN_NUM: return "TOKEN_NUM"; break;
+        case TOKEN_EQUALS: return "TOKEN_EQUALS"; break;
+        case TOKEN_SEMI: return "TOKEN_SEMI"; break;
+        case TOKEN_LPAREN: return "TOKEN_LPAREN"; break;
+        case TOKEN_RPAREN: return "TOKEN_RPAREN"; break;
+        case TOKEN_ADD: return "TOKEN_ADD"; break;
+        case TOKEN_HYPHEN: return "TOKEN_HYPHEN"; break;
+        case TOKEN_COLON: return "TOKEN_COLON"; break;
+        default: return "TOKEN_UNIDENTIFIED";
+    }
+}
