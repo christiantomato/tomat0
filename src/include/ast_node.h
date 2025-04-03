e001430bead67ad8da2ced8714c6df54a76c505f
christiantomato <christianntamayo@gmail.com>
2025-04-03 13:30:31 -0400

    starting to define a node for the AST

---
+++ src/include/ast_node.h
@@ -0,0 +1,28 @@
+//define nodes for the abstract syntax tree
+/*
+- variable assignments
+- factors vs terms
+- identifiers
+- statements
+*/
+
+#ifndef AST_NODE
+#define AST_NODE
+
+//define the enum for all the different types of nodes in the syntax tree
+typedef enum ast_node_type {
+    AST_DECLARATION,
+    AST_FUNCTION_CALL,
+    AST_STATEMENTS
+} type;
+
+//each node will be of the form (type, children[])
+typedef struct ast_node_struct {
+    //ast node type
+    int type;
+    //have a reference to the children (differs depending on the specific type of ast node)
+    
+} ast_node;
+
+
+#endif
