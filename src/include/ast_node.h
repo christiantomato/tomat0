109231c30175a0813dcb22725172415427982920
christiantomato <christianntamayo@gmail.com>
2025-04-16 23:08:50 -0400

    ast visualization, rigourously defining nodes, am i cooking?

--- src/include/ast_node.h
+++ src/include/ast_node.h
@@ -14,44 +14,44 @@
 //define the enum for all the different types of nodes that are possible in the syntax tree
 typedef enum ast_node_type {
     AST_PROGRAM,
-    AST_FUNCTION_CALL,
     AST_VARIABLE_DECLARATION,
-    AST_VARIABLE,
-    AST_BINARY_OPR,
-    AST_STRING,
-} type;
+    AST_BINARY_OP,
+    AST_INTEGER,
+} NodeType;
 
-//define all the different types of ast nodes with their arguments and properties
-typedef struct ast_program_node {
-    //program node properties
-} program_node;
-typedef struct ast_function_node {
-    //function properties
-    char* function_name;
+//forward declare our AST node structure
+struct ast_node_struct;
 
-} function_node;
-typedef struct ast_var_declaration_node {
-    //variable declaration properties
+//define all the different type of ast nodes that have special properties and attributes
+typedef struct ast_variable_declaration_node {
+    char* variable_type;
     char* variable_name;
+    struct ast_node_struct* assignment;
+} VariableDeclarationNode;
+typedef struct ast_binary_operation {
+    struct ast_node_struct* left;
+    struct ast_node_struct* right;
+    char* operand;
+} BinaryOperationNode;
+typedef struct ast_int_node {
+    int value;
+} IntegerNode;
 
-} declaration_node;
+//unionize all the different node types
+typedef union ast_node_structs {
+    VariableDeclarationNode declaration_node;
+    BinaryOperationNode binary_operation_node;
+    IntegerNode integer_node;
+} SpecializedNode;
 
-//unionize all the different types of node types
-typedef union ast_node_types {
-    //include all types
-    program_node program_node;
-    function_node function_node;
-    declaration_node declaration_node;
-} node_type;
-
-//each node will be of the form (type, children[])
+//fully declare the ast node - each node will be of the form (type, children[], node data (arguments, properties, etc.))
 typedef struct ast_node_struct {
     //type of node using enum
-    int type;
+    NodeType type;
     //list of children nodes
-    list* children;
-    //the union which will contain all specfic data about the actual type of node
-    node_type node_data;
-} ast_node;
+    List* children;
+    //utilizing the union, we can assign the correct data to the type of node
+    SpecializedNode attributes;
+} ASTNode;
 
 #endif
