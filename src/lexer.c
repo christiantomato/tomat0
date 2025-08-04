9d4ac7fbe7d25f248a8bfc3a64e9b5a19b0af236
christiantomato <christianntamayo@gmail.com>
2025-08-03 23:55:30 -0400

    rename and caught sneaky logic bug in skipwhitespace

--- src/lexer.c
+++ src/lexer.c
@@ -1,39 +1,58 @@
-//implement the lexer with the specified functions
 #include "include/lexer.h"
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-//lexer initializor
+/*
+Initialize Lexer Function
+
+creates a lexer and passes in the contents for it to analyze
+
+char* contents: the string of text to analyze (read from the .tmt file)
+
+return: pointer to the created lexer
+*/
+
 Lexer* init_lexer(char* contents) {
     //allocate memory for the lexer
     Lexer* lexer = malloc(sizeof(Lexer));
-    //initialize values 
-    lexer->i = 0;
-    lexer->c = contents[lexer->i];
+    //set up initial values
+    lexer->index = 0;
+    lexer->curr = contents[lexer->index];
     lexer->contents = contents;
     return lexer;
 }
 
-//move to the next character
+/*
+Lexer Advance Function
+
+advances to the next character in the contents
+
+Lexer* lexer: the lexer that is advanced
+*/
+
 void lexer_advance(Lexer* lexer) {
     //if we are still in string and not at null character
-    if(lexer->c != '\0' && lexer->i < strlen(lexer->contents)) {
+    if(lexer->curr != '\0' && lexer->index < strlen(lexer->contents)) {
         //increment the index
-        lexer->i++;
+        lexer->index++;
         //move to the next character
-        lexer->c = lexer->contents[lexer->i];
+        lexer->curr = lexer->contents[lexer->index];
     }
 }
 
-//lexer skip over whitespace 
-void skip_whitespace(Lexer* lexer) {
-    //advance until no longer on whitespace, but make sure not to skip past newlines!
-    if(lexer->c == '\n') {
-        //new line so don't advance the lexer!
-        return;
-    }
-    while(isspace(lexer->c)) {
+/*
+Lexer Skip Whitespace Function
+
+skips any blank space between characters as they are irrelvant, 
+but takes into account new lines as tomat0 uses them as delimeters
+
+Lexer* lexer: the lexer to skip whitespace
+*/
+
+void lexer_skip_whitespace(Lexer* lexer) {
+    //skip blank space - but not newlines!
+    while(isspace(lexer->curr) && lexer->curr != '\n') {
         lexer_advance(lexer);
     }
 }
@@ -41,39 +60,39 @@ void skip_whitespace(Lexer* lexer) {
 //critical method that will classify tokens
 Token* tokenize_next(Lexer* lexer) {
     //while we are not on null character and are still within contents
-    while(lexer->c != '\0' && lexer->i < strlen(lexer->contents)) {
+    while(lexer->curr != '\0' && lexer->index < strlen(lexer->contents)) {
         //first skip any whitespace
-        skip_whitespace(lexer);
+        lexer_skip_whitespace(lexer);
 
         //check for number first (variables cannot start with numbers in tomat0)
-        if(isdigit(lexer->c)) {
+        if(isdigit(lexer->curr)) {
             //tokenize the entire number
             return tokenize_number(lexer);
         }
         //for variables, keywords
-        if(isalpha(lexer->c)) {
+        if(isalpha(lexer->curr)) {
             //tokenize the variable/keyword
             return tokenize_ID(lexer);
         }
         
         //use a switch paired with the token type enum
-        switch(lexer->c) {
+        switch(lexer->curr) {
             case '"':
                 //since we have a quotation, we need to tokenize the string following it
                 return tokenize_string(lexer); break;
             //all other easy punctuations
-            case '\n': return continue_with_token(lexer, init_token(TOKEN_NEWLINE, lexer_char_as_string(lexer))); break;
-            case '=': return continue_with_token(lexer, init_token(TOKEN_EQUALS, lexer_char_as_string(lexer))); break;
-            case ';': return continue_with_token(lexer, init_token(TOKEN_SEMI, lexer_char_as_string(lexer))); break;
-            case '(': return continue_with_token(lexer, init_token(TOKEN_LPAREN, lexer_char_as_string(lexer))); break;
-            case ')': return continue_with_token(lexer, init_token(TOKEN_RPAREN, lexer_char_as_string(lexer))); break;
-            case '<': return continue_with_token(lexer, init_token(TOKEN_LCHEVRON, lexer_char_as_string(lexer))); break;
-            case '>': return continue_with_token(lexer, init_token(TOKEN_RCHEVRON, lexer_char_as_string(lexer))); break;
-            case '/': return continue_with_token(lexer, init_token(TOKEN_FSLASH, lexer_char_as_string(lexer))); break;
-            case '\\': return continue_with_token(lexer, init_token(TOKEN_BSLASH, lexer_char_as_string(lexer))); break;
-            case '+': return continue_with_token(lexer, init_token(TOKEN_PLUS, lexer_char_as_string(lexer))); break;
-            case '-': return continue_with_token(lexer, init_token(TOKEN_HYPHEN, lexer_char_as_string(lexer))); break;
-            case '*': return continue_with_token(lexer, init_token(TOKEN_ASTERIK, lexer_char_as_string(lexer))); break;
+            case '\n': return continue_with_token(lexer, init_token(TOKEN_NEWLINE, lexer_char_as_str(lexer))); break;
+            case '=': return continue_with_token(lexer, init_token(TOKEN_EQUALS, lexer_char_as_str(lexer))); break;
+            case ';': return continue_with_token(lexer, init_token(TOKEN_SEMI, lexer_char_as_str(lexer))); break;
+            case '(': return continue_with_token(lexer, init_token(TOKEN_LPAREN, lexer_char_as_str(lexer))); break;
+            case ')': return continue_with_token(lexer, init_token(TOKEN_RPAREN, lexer_char_as_str(lexer))); break;
+            case '<': return continue_with_token(lexer, init_token(TOKEN_LCHEVRON, lexer_char_as_str(lexer))); break;
+            case '>': return continue_with_token(lexer, init_token(TOKEN_RCHEVRON, lexer_char_as_str(lexer))); break;
+            case '/': return continue_with_token(lexer, init_token(TOKEN_FSLASH, lexer_char_as_str(lexer))); break;
+            case '\\': return continue_with_token(lexer, init_token(TOKEN_BSLASH, lexer_char_as_str(lexer))); break;
+            case '+': return continue_with_token(lexer, init_token(TOKEN_PLUS, lexer_char_as_str(lexer))); break;
+            case '-': return continue_with_token(lexer, init_token(TOKEN_HYPHEN, lexer_char_as_str(lexer))); break;
+            case '*': return continue_with_token(lexer, init_token(TOKEN_ASTERIK, lexer_char_as_str(lexer))); break;
             default: return NULL;
         }
     }
@@ -89,9 +108,9 @@ Token* tokenize_string(Lexer* lexer) {
     //make sure no garbage values, set null character
     strValue[0] = '\0';
     //build the string as long as we have not reached next quotation
-    while(lexer->c != '"') {
+    while(lexer->curr != '"') {
         //create a temp char that will store the current char as a string, so we can concat it later
-        char* temp = lexer_char_as_string(lexer);
+        char* temp = lexer_char_as_str(lexer);
         //reallocating memory as needed, since adding the next char (+2 for new char and null terminator which isn't counted)
         strValue = realloc(strValue, (strlen(strValue) + 2) * sizeof(char));
         //concatenate it
@@ -114,9 +133,9 @@ Token* tokenize_ID(Lexer* lexer) {
     //make sure no garbage values, set null character
     strValue[0] = '\0';
     //build the string as long as our ID is alpha numeric (therefore variable names are only alphanumeric!!!)
-    while(isalnum(lexer->c)) {
+    while(isalnum(lexer->curr)) {
         //create a temp char that will store the current char as a string, so we can concat it later
-        char* temp = lexer_char_as_string(lexer);
+        char* temp = lexer_char_as_str(lexer);
         //reallocating memory as needed, since adding the next char (+2 for new char and null terminator which isn't counted)
         strValue = realloc(strValue, (strlen(strValue) + 2) * sizeof(char));
         //concatenate it
@@ -153,9 +172,9 @@ Token* tokenize_number(Lexer* lexer) {
     //make sure no garbage values, set null character
     strValue[0] = '\0';
     //build the string as long as we are still on a number
-    while(isdigit(lexer->c)) {
+    while(isdigit(lexer->curr)) {
         //create a temp char that will store the current char as a string, so we can concat it later
-        char* temp = lexer_char_as_string(lexer);
+        char* temp = lexer_char_as_str(lexer);
         //reallocating memory as needed, since adding the next char (+2 for new char and null terminator which isn't counted)
         strValue = realloc(strValue, (strlen(strValue) + 2) * sizeof(char));
         //concatenate it
@@ -177,15 +196,15 @@ Token* continue_with_token(Lexer* lexer, Token* token) {
 }
 
 //method to get the current char as a char[]
-char* lexer_char_as_string(Lexer* lexer) {
+char* lexer_char_as_str(Lexer* lexer) {
     //special case for new line
-    if(lexer->c == '\n') {
+    if(lexer->curr == '\n') {
         return strdup("\\n");
     }
     //lexer->c is char, we need to make it a proper null terminated string
     char* char_as_str = malloc(2*sizeof(char));
     //build the null terminated string
-    char_as_str[0] = lexer->c;
+    char_as_str[0] = lexer->curr;
     char_as_str[1] = '\0';
     //return the char as a string
     return char_as_str;
