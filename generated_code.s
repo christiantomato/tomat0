6f4d4511b9d18aa406cb23d43cde8a5f4910beef
christiantomato <christianntamayo@gmail.com>
2025-09-04 15:23:11 -0400

    trying to print

--- generated_code.s
+++ generated_code.s
@@ -3,5 +3,7 @@
 .align 2
 
 _main:
+	mov w0, #10
+	bl _printf
 	mov w0, #0
 	ret
--- src/code_generator.c
+++ src/code_generator.c
@@ -12,13 +12,19 @@ int node_to_asm(FILE* file, ASTNode* node) {
     //switch on the node type
     switch(node->type) {
         case AST_PROGRAM:
-           
-            break;   
+            //go through children nodes
+            for(int i = 0; i < node->children->num_items; i++) {
+                node_to_asm(file, node->children->array[i]);
+            }
+            break;
         case AST_VARIABLE_DECLARATION:
    
             break;
         case AST_PRINT_STATEMENT:
-            
+            //generate code for what we are printing
+            node_to_asm(file, node->specialization.print_statement.statement);
+            //generate the assembly code for printing something to terminal
+            fprintf(file, "\tbl _printf\n");
             break;
         case AST_BINARY_OPERATION:
             
@@ -30,7 +36,8 @@ int node_to_asm(FILE* file, ASTNode* node) {
            
             break;
         case AST_INTEGER:
- 
+            //move value to register
+            fprintf(file, "\tmov w0, #%d\n", node->specialization.integer_literal.value);
             break;
         case AST_STRING:
          
@@ -48,13 +55,13 @@ int generate_assembly_code(FILE* file, ASTNode* root) {
     fprintf(file, ".align 2\n\n");
     fprintf(file, "_main:\n");
 
-    //recursive algorithm goes here (inorder traversal)
-
+    //recursive algorithm
+    node_to_asm(file, root);
 
 
     //exit
-    //fprintf(file, "\tmov w0, #0\n");
-    //fprintf(file, "\tret\n");
+    fprintf(file, "\tmov w0, #0\n");
+    fprintf(file, "\tret\n");
 
 
 
