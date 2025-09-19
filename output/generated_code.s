bf58b1ad55d4823ffec731ccb3157799037aabcf
christiantomato <christianntamayo@gmail.com>
2025-09-19 02:10:26 -0400

    compute a complicated arithmetic expression and print to terminal

--- output/generated_code.s
+++ output/generated_code.s
@@ -12,23 +12,35 @@ _main:
 	//Code Gen Starts Here: 
 
 	//move value to register
-	mov x0, #4
+	mov x0, #1
 	//move value to register
-	mov x1, #6
+	mov x1, #69
 	//move value to register
-	mov x2, #2
-	//binary operation
-	mul x3, x1, x2
-	//binary operation
-	add x1, x0, x3
+	mov x2, #420
 	//move value to register
-	mov x0, #8
+	mov x3, #21
 	//move value to register
-	mov x2, #3
+	mov x4, #19
 	//binary operation
-	sub x3, x0, x2
+	mul x5, x3, x4
 	//binary operation
-	add x0, x1, x3
+	sub x3, x2, x5
+	//binary operation
+	mul x2, x1, x3
+	//binary operation
+	add x1, x0, x2
+	//move value to register
+	mov x0, #1000
+	//move value to register
+	mov x2, #250
+	//binary operation
+	sdiv x3, x0, x2
+	//move value to register
+	mov x0, #284
+	//binary operation
+	add x2, x3, x0
+	//binary operation
+	add x0, x1, x2
 	//print
 	mov x8, x0
 	adr x0, integerformatstr
