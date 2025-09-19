b61aa23bbe2c651f1ed6e09f8533ae5353dfd45a
christiantomato <christianntamayo@gmail.com>
2025-09-19 15:01:26 -0400

    free function are a mess so lets leak memory for now

--- src/main.c
+++ src/main.c
@@ -38,8 +38,6 @@ int main (int argc, char *argv[]) {
     parser_parse(my_parser, my_table);
      //get a reference to the root node
     ASTNode* ast_tree_root = my_parser->root;
-    //free parser since tree is created
-    //free_parser(my_parser);
 
     //write ast representation from root now
     FILE* ast_file = fopen("output/ast_output.txt", "w");
@@ -52,10 +50,7 @@ int main (int argc, char *argv[]) {
     //close the file
     fclose(assembly_file);
 
-    //free the root node
-    //free_node(ast_tree_root);
-    //free the table
-    //free_symbol_table(my_table);
+    //got to work out how we are going to free everything... (currently in shambles)
     
     //make an executable
     system("gcc output/generated_asm.s -o tomat0executable");
